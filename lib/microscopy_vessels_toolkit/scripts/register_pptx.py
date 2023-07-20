4f027eba3080d4eaf1761f391cb9b2051aeb9ab1
gabriel.lepetitaimon <gabriel.lepetitaimon@gmail.com>
2023-07-20 13:47:38 -0400

    Implement saving to image in mvt register_pptx

--- lib/microscopy_vessels_toolkit/scripts/register_pptx.py
+++ lib/microscopy_vessels_toolkit/scripts/register_pptx.py
@@ -1,6 +1,8 @@
 from collections.abc import Container  # noqa
 from datetime import datetime
+from os.path import splitext
 
+import cv2
 from pptx import Presentation
 from pptx.shapes.group import GroupShape
 from pptx.shapes.picture import Picture
@@ -13,8 +15,10 @@ def register_pptx(pptx_path: str, output_path: str = None):
     prs = Presentation(pptx_path)
 
     if output_path is None:
-        pptx_name, pptx_ext = pptx_path.rsplit(".", 1)
-        output_path = pptx_name + "_registered." + pptx_ext
+        pptx_name, pptx_ext = splitext(pptx_path)
+        output_path = pptx_name + "_registered" + pptx_ext
+
+    output_is_ppt = splitext(output_path)[1] in (".ppt", ".pptx")
 
     for i, slide in enumerate(prs.slides):
         stitch = PatchStitching.from_pptx_slide(slide)
@@ -27,14 +31,25 @@ def register_pptx(pptx_path: str, output_path: str = None):
         t = datetime.now()
         stitch.crop_to_content()
         reg_stitch, reg_debug = register(stitch, return_debug=True, verbose=True)
-        relative_offset = {stitch[k].alias: v for k, v in reg_debug["relative_offset"].items()}
-        update_pictures_recursive(slide.shapes, relative_offset)
         print(f"\t... registered in {(datetime.now() - t).total_seconds():.1f} seconds.")
 
-    print(f"> Saving output to {output_path}...")
-    t = datetime.now()
-    prs.save(output_path)
-    print(f"\t... saved in {(datetime.now() - t).total_seconds():.1f} seconds.")
+        if output_is_ppt:
+            relative_offset = {stitch[k].alias: v for k, v in reg_debug["relative_offset"].items()}
+            update_pictures_recursive(slide.shapes, relative_offset)
+        else:
+            filename, ext = splitext(output_path)
+            slide_output_path = filename + f"_{i+1:02d}." + ext
+            print(f"\t saving slide to {slide_output_path}...")
+            t = datetime.now()
+            stitched_img = stitch.stitch()
+            cv2.imwrite(slide_output_path, stitched_img)
+            print(f"\t... saved in {(datetime.now() - t).total_seconds():.1f} seconds.")
+
+    if output_is_ppt:
+        print(f"> Saving output to {output_path}...")
+        t = datetime.now()
+        prs.save(output_path)
+        print(f"\t... saved in {(datetime.now() - t).total_seconds():.1f} seconds.")
 
 
 def update_pictures_recursive(group, relative_offset):
